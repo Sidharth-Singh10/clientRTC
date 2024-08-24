1269ad466f5e7507d8a488523475ccc6414a1665
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-08-24 18:09:36 +0530

    update

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,31 +1,60 @@
-import React, { useCallback, useEffect } from 'react'
-import { useSocket } from '../context/SocketProvider';
+import React, { useCallback, useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+import { useSocket } from "../context/SocketProvider";
+
+import peer from "../service/peer";
 
 const Room = () => {
+  const socket = useSocket();
 
-  const socket = useSocket ();
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
 
-  const  handleUserJoined = useCallback(({email,id})=>{
-    console.log(email );
+    const offer = await peer.getOffer();
 
-  },[])
+    socket.emit("user:call", { to: remoteSocketId, offer });
+    setMyStream(stream);
+  },[remoteSocketId,socket]);
 
-  useEffect(() =>{
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState(null);
 
-    socket.on('user:joined')
+  const handleUserJoined = useCallback(({ email, id }) => {
+    console.log(email);
+    setRemoteSocketId(id);
+  }, []);
 
-    return () =>{
-      socket.off('user:joined',handleUserJoined)
-    }
-  },[handleUserJoined,socket])
+  useEffect(() => {
+    socket.on("user:joined", handleUserJoined);
+
+    return () => {
+      socket.off("user:joined", handleUserJoined);
+    };
+  }, [handleUserJoined, socket]);
 
   return (
     <div>
-        <h1>
-            Room page
-        </h1>
-    </div>
-  )
-}
+      <h1>Room page</h1>
+      <h4>{remoteSocketId ? "Connected" : "Emprt room"} </h4>
 
-export default Room
+      <div>
+        <h1> your video </h1>
+        {remoteSocketId && <button onClick={handleCallUser}>CALL </button>}
+        {myStream && (
+          <ReactPlayer
+            playing
+            muted
+            height="300px"
+            width="500px"
+            url={myStream}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Room;
